ae07d45bc3b71ef7ffba39e9ce6384ff3e8afe61
Jason Worden <wordenjasonp@gmail.com>
2016-10-17 22:08:46 -0700

    Easily switch between Paperscript and plain JS

---
+++ js/sketch-paperscript.js
@@ -0,0 +1,24 @@
+// Create a circle shaped path at { x: 80, y: 50 }
+// with a radius of 35:
+var path = new Path.Circle({
+	center: [80, 50],
+	radius: 35
+});
+
+// Select the path, so we can inspect its segments:
+path.selected = true;
+
+// Create a copy of the path and move it by 150 points:
+var copy = path.clone();
+copy.position.x += 150;
+
+// Flatten the copied path, with a maximum error of 4 points:
+copy.flatten(.25);
+
+///////////// event handlers:
+
+function onFrame(event) {
+  if(window.stats && window.stats.update) {
+    window.stats.update();
+  }
+}
